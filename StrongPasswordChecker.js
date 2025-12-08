459dd58d3fc939048d837f1484bf9ac23cf7e0e7
Hemraj-7 <mhemraj814@gmail.com>
2025-12-09 00:40:23 +0530

    Problem 29 - Strong Password Checker

--- StrongPasswordChecker.js
+++ StrongPasswordChecker.js
@@ -55,3 +55,52 @@ console.log(isPasswordStrong("Hello!123"));
 console.log(isPasswordStrong("Hello!"));
 console.log(isPasswordStrong("Hello Ji 123"));
 console.log(isPasswordStrong("hellosarkar"));
+
+// -----------------------------------------------------------------------------------------------------------
+
+function isStrongPassword(passwordString) {
+  passwordString = passwordString.trim();
+  if (passwordString.length < 8) {
+    return false;
+  }
+
+  const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  const spacialChars = "!@";
+
+  let lowerCaseCharsExists = false;
+  let upperCaseCharsExists = false;
+  let numbersExists = false;
+  let spacialCharsExists = false;
+
+  for (let char of passwordString) {
+    console.log(char);
+    if (lowerCaseChars.includes(char)) {
+      lowerCaseCharsExists = true;
+    } else if (upperCaseChars.includes(char)) {
+      upperCaseCharsExists = true;
+    } else if (numbers.includes(char)) {
+      numbersExists = true;
+    } else if (spacialChars.includes(char)) {
+      spacialCharsExists = true;
+    }
+  }
+
+  console.log("lowerCaseCharsExists", lowerCaseCharsExists);
+  console.log("upperCaseCharsExists", upperCaseCharsExists);
+  console.log("numbersExists", numbersExists);
+  console.log("spacialCharsExists", spacialCharsExists);
+
+  if (
+    lowerCaseCharsExists &&
+    upperCaseCharsExists &&
+    numbersExists &&
+    spacialCharsExists
+  ) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isStrongPassword("HEmsa@123"));
