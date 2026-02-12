69839c130e671f5d4407406ee790971b50af77aa
Hemraj-7 <mhemraj814@gmail.com>
2026-02-12 15:23:49 +0530

    Problem 39 - Basic Calulator

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -52,4 +52,76 @@ function calculate() {
   }
 }
 
-calculate();
+// calculate();
+
+// Answered by Yt Video!!
+
+let exit = false;
+
+while (!exit) {
+  let userChoice = prompt(`Please select one of the below options
+  1. Press 1 for Addition
+  2. Press 2 for Subtraction
+  3. Press 3 for Multiplication
+  4. Press 4 for Division
+  5. Press 5 for Exit:
+  `);
+
+  userChoice = parseInt(userChoice);
+
+  switch (userChoice) {
+    case 1:
+      console.log("Addition");
+      const userInputForAddition = acceptTowNumbers();
+      const resultAddition =
+        userInputForAddition.firstNumber + userInputForAddition.secondNumber;
+      console.log(
+        `${userInputForAddition.firstNumber} + ${userInputForAddition.secondNumber} = ${resultAddition}`,
+      );
+      break;
+    case 2:
+      console.log("Subtraction");
+      const userInputForSubtraction = acceptTowNumbers();
+      const resultSubtraction =
+        userInputForSubtraction.firstNumber -
+        userInputForSubtraction.secondNumber;
+      console.log(
+        `${userInputForSubtraction.firstNumber} - ${userInputForSubtraction.secondNumber} = ${resultSubtraction}`,
+      );
+      break;
+    case 3:
+      console.log("Multiplication");
+      const userInputForMultiplication = acceptTowNumbers();
+      const resultMultiplication =
+        userInputForMultiplication.firstNumber *
+        userInputForMultiplication.secondNumber;
+      console.log(
+        `${userInputForMultiplication.firstNumber} * ${userInputForMultiplication.secondNumber} = ${resultMultiplication}`,
+      );
+      break;
+    case 4:
+      console.log("Division");
+      const userInputForDivision = acceptTowNumbers();
+      const resultDivision =
+        userInputForDivision.firstNumber / userInputForDivision.secondNumber;
+      console.log(
+        `${userInputForDivision.firstNumber} / ${userInputForDivision.secondNumber} = ${resultDivision}`,
+      );
+      break;
+    case 5:
+      console.log("Exit");
+      exit = true;
+      break;
+    default:
+      console.log("Wrong Choice");
+      break;
+  }
+}
+
+console.log("Thanks for using the calculator!")
+
+function acceptTowNumbers() {
+  let firstNumber = parseFloat(prompt("Please Enter the first number: "));
+  let secondNumber = parseFloat(prompt("Please Enter the second number: "));
+  return { firstNumber, secondNumber };
+}
