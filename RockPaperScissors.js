5bd9038939be0a0c11347dce6bd9e82c77c22c5e
Hemraj-7 <mhemraj814@gmail.com>
2026-02-13 19:31:05 +0530

    Problem 40 - Rock Paper Scissors Game

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,5 +1,7 @@
 const prompt = require("prompt-sync")();
 
+// Solution by Me!! ->
+
 let exit = false;
 
 while (!exit) {
@@ -76,3 +78,69 @@ while (!exit) {
       break;
   }
 }
+
+// Solution by YT Video!! ->
+
+const rock = 1;
+const paper = 2;
+const scissors = 3;
+
+const names = {
+  1: "ROCK",
+  2: "PAPER",
+  3: "SCISSORS",
+};
+
+let exits = false;
+let userScore = 0;
+let computerScore = 0;
+
+while (!exits) {
+  const computerChoice = Math.floor(Math.random() * 3) + 1;
+
+  console.log(computerChoice);
+
+  const userChoice = parseInt(
+    prompt(`Please select one of the below options
+  1. Press 1 for ROCK
+  2. Press 2 for PAPER
+  3. Press 3 for SCISSORS
+  4. Press 4 for Exit:
+  `),
+  );
+
+  if (userChoice === 4) {
+    exits = true;
+  } else if (!Number.isInteger(userChoice) || userChoice < 1 || userChoice > 4) {
+    console.log("Invalid Choice!! Try Again!");
+  } else if (userChoice === computerChoice) {
+    console.log("Its a tie!! Your Both choice is same : ", names[userChoice]);
+  } else if (
+    (userChoice === rock && computerChoice === scissors) ||
+    (userChoice === paper && computerChoice === rock) ||
+    (userChoice === scissors && computerChoice === paper)
+  ) {
+    userScore++;
+    console.log(
+      "You Win!!",
+      "Your choice is : ",
+      names[userChoice],
+      "& Computer choice is : ",
+      names[computerChoice],
+    );
+  } else {
+    computerScore++;
+    console.log(
+      "Computer Win!!",
+      "Your choice is : ",
+      names[userChoice],
+      "& Computer choice is : ",
+      names[computerChoice],
+    );
+  }
+
+  console.log(computerChoice, userChoice);
+}
+
+console.log("Thanks for Playing!!");
+console.log("End Score is User: ", userScore, " Computer: ", computerScore);
